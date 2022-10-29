aefaab8f0f5444937ce3a7642a6e103e07485c25
Alvin Mutebi <36954843+alvienzo720@users.noreply.github.com>
2022-10-29 18:14:57 +0300

    Change format in jason response

--- StageOneTask/base/views.py
+++ StageOneTask/base/views.py
@@ -18,5 +18,5 @@ class InternList(APIView):
 
     def get(self, request):
 
-        data = {'slackUsername': 'String','backend': 'true','age':'28','bio':'String'}
+        data = {"slackUsername":"Alvin Mutebi","backend":"True","age":"28","bio":"Iam a python Developer"}
         return Response(data)
